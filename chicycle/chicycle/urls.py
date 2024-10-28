2ebafc8154cf5a887c4d83569bf41c77297c1f4f
wiem hamdoun <wiem.hamdoun@esprit.tn>
2024-10-28 12:56:52 +0100

    avis et reponse

--- chicycle/chicycle/urls.py
+++ chicycle/chicycle/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for chicycle project.
 
@@ -14,16 +15,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-from django.contrib import admin
-from django.urls import path
 from users import views
+from users.viewsAvis import creer_avis, modifier_avis, supprimer_avis, liste_avis ,user_avis,detail_avis ,creer_reponse,modifier_reponse,supprimer_reponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,8 +30,21 @@ urlpatterns = [
     path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('register/', views.register_view, name='register'),  # URL d'inscription
     path('logout/', LogoutView.as_view(), name='logout'),  # URL pour la déconnexion
-  path('users/', views.get_all_users, name='get_all_users'),  # URL pour la gestion des utilisateurs
+    
+    path('creer/', creer_avis, name='creer_avis'),
+    path('modifier/<int:avis_id>/', modifier_avis, name='modifier_avis'),
+    path('supprimer/<int:avis_id>/', supprimer_avis, name='supprimer_avis'),
+    path('mes-avis/', user_avis, name='user_avis'),
+            # Routes pour les réponses
+    path('avis/<int:avis_id>/reponse/creer/', creer_reponse, name='creer_reponse'),
+    path('reponse/<int:reponse_id>/modifier/', modifier_reponse, name='modifier_reponse'),
+    path('reponse/<int:reponse_id>/supprimer/', supprimer_reponse, name='supprimer_reponse'),
+    path('liste/', liste_avis, name='liste_avis'), 
+    path('avis/<int:avis_id>/', detail_avis, name='detail_avis'),
+    path('users/', views.get_all_users, name='get_all_users'),  # URL pour la gestion des utilisateurs
     path('users/update/<int:user_id>/', views.update_user, name='update_user'),  # URL pour mettre à jour un utilisateur
     path('users/ban/<int:user_id>/', views.ban_user, name='ban_user'),  # URL pour bannir un utilisateur
     path('users/unban/<int:user_id>/', views.unban_user, name='unban_user'),  # URL pour débannir un utilisateur
+
+
 ]
--- chicycle/users/forms.py
+++ chicycle/users/forms.py
@@ -1,9 +1,36 @@
 from django import forms
 from .models import CustomUser
-
+from .models import Avis
+from .models import Reponse
+from django.core.exceptions import ValidationError
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password', 'role']
+
+
+class AvisForm(forms.ModelForm):
+    class Meta:
+        model = Avis
+        fields = ['produit', 'commentaire', 'note']
+        widgets = {
+            'note': forms.HiddenInput(),  
+        }
+
+    def clean_commentaire(self):
+        commentaire = self.cleaned_data.get('commentaire')
+        if len(commentaire) < 4:
+            raise ValidationError('The comment must contain at least 4 characters.')
+        return commentaire
+
+class ReponseForm(forms.ModelForm):
+    class Meta:
+        model = Reponse
+        fields = ['commentaire']
+    def clean_commentaire(self):
+        commentaire = self.cleaned_data.get('commentaire')
+        if len(commentaire) < 5:
+            raise ValidationError('The response must contain at least 5 characters.')
+        return commentaire
